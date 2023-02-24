e1baee79ff90956cf8a76fa93feb63a4604ec9eb
hamed <h.makled32@gmail.com>
2023-02-24 04:23:48 +0200

    first commit

---
+++ tast.js
@@ -0,0 +1,11 @@
+var string= prompt("Enter operator to perform the calculation (+, -, *, /)");
+var name1= prompt("frist number")
+var name2=prompt("second number");
+if (string== '+')
+alert (+name1 + +name2);
+if (string== '-')
+alert(name1-name2);
+if (string== '*')
+alert(name1*name2);
+if (string== '/')
+alert(name1/name2);
